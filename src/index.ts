52731965e0d5cc84eddc217fb4082e9fd4ce2582
umokay1021 <mykrsk1021@gmail.com>
2023-06-18 23:30:57 +0900

    環境変数

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello world')
+import axios from 'axios';
+
+async function postData(url: string, data: any) {
+    try {
+        const response = await axios.post(url, data);
+        console.log(response.data);
+    } catch (error) {
+        console.error(`Error posting data to ${url}: `, error);
+    }
+}
+
+const url = process.env.WEBHOOK_URL
+
+if (!url) {
+    console.log('Please set WEBHOOK_URL')
+} else {
+    postData(url, { content: 'kazuki' });
+}
+
