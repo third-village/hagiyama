470715dd7c1bb7b19c56e07ba45e02b62a7c88e6
Kazuki Matsumaru <marukaz.jh@gmail.com>
2023-06-24 14:52:02 +0000

    Format code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 async function postData(url: string, data: any) {
-    try {
-        const response = await axios.post(url, data);
-        console.log(response.data);
-    } catch (error) {
-        console.error(`Error posting data to ${url}: `, error);
-    }
+  try {
+    const response = await axios.post(url, data);
+    console.log(response.data);
+  } catch (error) {
+    console.error(`Error posting data to ${url}: `, error);
+  }
 }
-
-const url = process.env.WEBHOOK_URL
+const url = process.env.WEBHOOK_URL;
 
 if (!url) {
-    console.log('Please set WEBHOOK_URL')
+  console.log("Please set WEBHOOK_URL");
 } else {
-    postData(url, { content: 'kazuki' });
+  postData(url, { content: "mayuko" });
 }
-
